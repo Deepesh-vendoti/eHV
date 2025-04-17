e8ad0fd832edf277d7768dd61d7145a16e4406a1
Deepesh <deepesh.vendoti@gmail.com>
2025-04-17 13:01:22 +0530

    Version with Store functioning at FE and APIs as theye were already there

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+=======
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+} 
+>>>>>>> c5fbe063 (Version with Store functioning at FE and APIs as theye were already there)
